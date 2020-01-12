784837b25d4b15928c58fee27d808c2af63ef755
Kasyanov Ruslan <ri.kasyanov@gmail.com>
2020-01-12 13:22:30 +0300

    Initial Commit

---
+++ RealmPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmPlaces
+//
+//  Created by User on 12/01/2020.
+//  Copyright © 2020 Ruslan Kasyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
